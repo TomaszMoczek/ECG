ed3c25c04acea6fba2fbc29cfa33c34d5fbcb747
Tomasz Moczek <t.moczek@gmail.com>
2023-12-01 21:31:07 +0100

    changes added

--- ecg.py
+++ ecg.py
@@ -1,3 +1,11 @@
 class Ecg:
     def __init__(self) -> None:
+        self.fs = 360
+        self.gain = 200
+        self.base = 1024
         pass
+
+    async def get_data(self, file_path: str) -> tuple(list, list):
+        mlii = []
+        v1 = []
+        return mlii, v1
