93481fc8e82b8f5f1b5f8b96303ab263e8b8d92d
Tomasz Moczek <t.moczek@gmail.com>
2023-12-02 23:37:29 +0100

    changes added

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 from ecg import Ecg
 from dspplotter import DspPlotter
+import pygame
 import numpy
 import wave
+import time
 import os
 
 
@@ -14,6 +16,21 @@ def write_wave_file(data: list, fs: int, file_path: str) -> None:
         file.setsampwidth(2)
         file.setframerate(fs)
         file.writeframes(audio.tobytes())
+
+
+def init_wave_player(fs: int) -> None:
+    pygame.mixer.init(frequency=fs)
+
+
+def play_wave_file(file_path: str) -> None:
+    pygame.mixer.music.load(file_path)
+    pygame.mixer.music.play()
+
+
+def stop_wave_file() -> None:
+    if pygame.mixer.music.get_busy():
+        pygame.mixer.music.stop()
+    pygame.mixer.music.unload()
 
 
 def main() -> None:
@@ -31,8 +48,18 @@ def main() -> None:
         file_path=os.path.join(os.path.dirname(os.path.abspath(__file__)), "v1.wav"),
     )
     dsp_plotter = DspPlotter()
+    init_wave_player(fs=fs)
+    play_wave_file(
+        file_path=os.path.join(os.path.dirname(os.path.abspath(__file__)), "mlii.wav")
+    )
     dsp_plotter.plot(data=mlii, title="MLII", Fs=fs, freqresp=False)
+    stop_wave_file()
+    time.sleep(1.0)
+    play_wave_file(
+        file_path=os.path.join(os.path.dirname(os.path.abspath(__file__)), "v1.wav")
+    )
     dsp_plotter.plot(data=v1, title="V1", Fs=fs, freqresp=False)
+    stop_wave_file()
 
 
 if __name__ == "__main__":
