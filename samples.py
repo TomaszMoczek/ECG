dd46f6776ff26ef3f4e7b490ac748eceadcb1b9d
Tomasz Moczek <tomasz.moczek@kantar.com>
2024-07-17 16:44:18 +0200

    changes added

--- samples.py
+++ samples.py
@@ -1,44 +1,28 @@
 import os
 import numpy
+import pandas
 import wave
 
 
 class Samples:
     def __init__(self) -> None:
-        self.__fs = 360
-        self.__mlii = []
-        self.__v1 = []
+        pass
 
     def get_fs(self) -> int:
-        return self.__fs
+        return 360
 
-    def get_data(self) -> tuple:
-        if len(self.__mlii) == 0 and len(self.__v1) == 0:
-            index = -1
-            with open(
-                os.path.join(os.path.dirname(os.path.abspath(__file__)), "samples.csv"),
-                "r",
-            ) as file:
-                lines = file.readlines()
-            for line in lines:
-                index += 1
-                if index <= 1:
-                    continue
-                items = line.rstrip("\n").split(",")
-                self.__mlii.append(float(items[1]))
-                self.__v1.append(float(items[2]))
-        return self.__mlii, self.__v1
+    def get_data(self) -> numpy.ndarray:
+        file_path: str = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)), "samples.csv"
+        )
+        data_frame: pandas.DataFrame = pandas.read_csv(file_path, header=[0, 1])
+        data: numpy.ndarray = data_frame.to_numpy()
+        return data[:, [1, 2]]
 
-    def write_wave_file(fs: int, data: tuple, file_path: str) -> bool:
-        if (
-            len(data) != 2
-            or len(data[0]) == 0
-            or len(data[1]) == 0
-            or len(data[0]) != len(data[1])
-        ):
+    def write_wave_file(fs: int, data: numpy.ndarray, file_path: str) -> bool:
+        if data.shape != (3600, 2) or data.size != 7200:
             return False
-        audio = numpy.array([data[1], data[0]]).T
-        audio = (audio * (2**15 - 1)).astype("<h")
+        audio = (data * (2**15 - 1)).astype("<h")
         with wave.open(file_path, "w") as file:
             file.setnchannels(2)
             file.setsampwidth(2)
