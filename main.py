9bcd934a47602a24ab20d6931795479568ea31c1
Tomasz Moczek <t.moczek@gmail.com>
2024-10-30 20:47:16 +0100

    changes added

--- main.py
+++ main.py
@@ -54,6 +54,7 @@ def plot_signal(fs: int, data: numpy.ndarray, labels: tuple, file_name: str) -> 
 
 
 def main() -> None:
+    global is_iir
     global is_signal
 
     samples = Samples()
@@ -82,36 +83,53 @@ def main() -> None:
             file_name="samples-wiener-filtered.wav",
         )
 
-    w0 = 60.0
-    bw = 2.0
-    b, a = signal.iirnotch(w0=w0, Q=w0 / bw, fs=float(fs))
-    y1 = signal.lfilter(b=b, a=a, x=x)
-    y4 = signal.lfilter(b=b, a=a, x=x)
-    data[0] = signal.filtfilt(b=b, a=a, x=data[0])
-    data[1] = signal.filtfilt(b=b, a=a, x=data[1])
+    if is_iir:
+        b, a = signal.iirnotch(w0=60.0, Q=(60.0 / 2.0), fs=float(fs))
+        y1 = signal.lfilter(b=b, a=a, x=x)
+        y4 = signal.lfilter(b=b, a=a, x=x)
+        data[0] = signal.filtfilt(b=b, a=a, x=data[0])
+        data[1] = signal.filtfilt(b=b, a=a, x=data[1])
+    else:
+        b, a = signal.iirnotch(w0=60.0, Q=(60.0 / 2.0), fs=float(fs))
+        y1 = signal.lfilter(b=b, a=a, x=x)
+        y4 = signal.lfilter(b=b, a=a, x=x)
+        data[0] = signal.filtfilt(b=b, a=a, x=data[0])
+        data[1] = signal.filtfilt(b=b, a=a, x=data[1])
 
-    wn = 30.0
-    sos = signal.butter(N=2, Wn=wn, btype="lowpass", output="sos", fs=float(fs))
-    y2 = signal.sosfilt(sos=sos, x=x)
-    y4 = signal.sosfilt(sos=sos, x=y4)
-    data[0] = signal.sosfiltfilt(sos=sos, x=data[0])
-    data[1] = signal.sosfiltfilt(sos=sos, x=data[1])
+    if is_iir:
+        sos = signal.butter(N=2, Wn=30.0, btype="lowpass", output="sos", fs=float(fs))
+        y2 = signal.sosfilt(sos=sos, x=x)
+        y4 = signal.sosfilt(sos=sos, x=y4)
+        data[0] = signal.sosfiltfilt(sos=sos, x=data[0])
+        data[1] = signal.sosfiltfilt(sos=sos, x=data[1])
+    else:
+        sos = signal.butter(N=2, Wn=30.0, btype="lowpass", output="sos", fs=float(fs))
+        y2 = signal.sosfilt(sos=sos, x=x)
+        y4 = signal.sosfilt(sos=sos, x=y4)
+        data[0] = signal.sosfiltfilt(sos=sos, x=data[0])
+        data[1] = signal.sosfiltfilt(sos=sos, x=data[1])
 
-    wn = 0.5
-    sos = signal.butter(N=2, Wn=wn, btype="highpass", output="sos", fs=float(fs))
-    y3 = signal.sosfilt(sos=sos, x=x)
-    y4 = signal.sosfilt(sos=sos, x=y4)
-    # data[0] = signal.sosfiltfilt(sos=sos, x=data[0])
-    # data[1] = signal.sosfiltfilt(sos=sos, x=data[1])
+    if is_iir:
+        sos = signal.butter(N=2, Wn=0.5, btype="highpass", output="sos", fs=float(fs))
+        y3 = signal.sosfilt(sos=sos, x=x)
+        y4 = signal.sosfilt(sos=sos, x=y4)
+        # data[0] = signal.sosfiltfilt(sos=sos, x=data[0])
+        # data[1] = signal.sosfiltfilt(sos=sos, x=data[1])
+    else:
+        sos = signal.butter(N=2, Wn=0.5, btype="highpass", output="sos", fs=float(fs))
+        y3 = signal.sosfilt(sos=sos, x=x)
+        y4 = signal.sosfilt(sos=sos, x=y4)
+        # data[0] = signal.sosfiltfilt(sos=sos, x=data[0])
+        # data[1] = signal.sosfiltfilt(sos=sos, x=data[1])
 
     dsp_plotter.plot(
         fs=fs,
         data=numpy.vstack((y1, y2, y3, y4)),
         labels=(
-            "IIR notch 60 Hz",
-            "IIR low-pass 30 Hz",
-            "IIR high-pass 0.5 Hz",
-            "IIR cascaded",
+            "IIR notch 60 Hz" if is_iir else "FIR band-stop 60 Hz",
+            "IIR low-pass 30 Hz" if is_iir else "FIR low-pass 30 Hz",
+            "IIR high-pass 0.5 Hz" if is_iir else "FIR high-pass 0.5 Hz",
+            "IIR cascaded" if is_iir else "FIR cascaded",
         ),
         phaseresp=True,
     )
@@ -120,15 +138,21 @@ def main() -> None:
         plot_signal(
             fs=fs,
             data=data,
-            labels=("MLII [IIR filtered]", "V1 [IIR filtered]"),
-            file_name="samples-iir-filtered.wav",
+            labels=(
+                "MLII [IIR filtered]" if is_iir else "MLII [FIR filtered]",
+                "V1 [IIR filtered]" if is_iir else "V1 [FIR filtered]",
+            ),
+            file_name=(
+                "samples-iir-filtered.wav" if is_iir else "samples-fir-filtered.wav"
+            ),
         )
 
 
 if __name__ == "__main__":
     config = configparser.ConfigParser()
     config.read(os.path.join(os.path.dirname(os.path.abspath(__file__)), "config.ini"))
-    is_signal = config.getboolean("default", "signal")
+    is_iir = config.getboolean("default", "iir")
     is_sound = config.getboolean("default", "sound")
+    is_signal = config.getboolean("default", "signal")
     is_spectrogram = config.getboolean("default", "spectrogram")
     main()
